f17ceb756509d72aeef3191c8a3c28acb51786ff
Denis <dewy.dyks@gmail.com>
2026-06-27 09:34:39 +0300

    mod 27

--- src/player.cpp
+++ src/player.cpp
@@ -338,8 +338,7 @@ void destroy_renderer(const Renderer &renderer)
 std::vector<carrot::ImageRgb> load_decoded_frames(const std::string &folder)
 {
     const std::vector<carrot::MjpegFrame> encoded_frames = carrot::encode_folder(folder, 100);
-    std::filesystem::create_directories("media");
-    carrot::write_mjpeg_stream("media/video.mjpeg", encoded_frames);
+    carrot::write_mjpeg_stream("media/out/video.mjpeg", encoded_frames);
     const carrot::MjpegDecoder decoder;
     std::vector<carrot::ImageRgb> decoded_frames;
     decoded_frames.reserve(encoded_frames.size());
@@ -378,8 +377,7 @@ int carrot::run_player(int argc, char **argv)
         const carrot::ImaAdpcmDecoder audio_decoder;
         const std::vector<carrot::ImaAdpcmBlock> encoded_audio = audio_encoder.encode(wav.samples,
                                                                                       wav.channels);
-        std::filesystem::create_directories("media");
-        carrot::write_ima_adpcm_stream("media/audio.adpcm",
+        carrot::write_ima_adpcm_stream("media/out/audio.adpcm",
                                        encoded_audio,
                                        wav.channels,
                                        wav.sample_rate);
